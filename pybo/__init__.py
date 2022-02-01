282ab04e24e69e87249fe7a1ca3eea60b8ceb603
Lee Sang Jin <lee3jjang@gmail.com>
2022-02-02 01:25:53 +0900

    add 404

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -17,5 +20,7 @@ def create_app():
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+
+    app.register_error_handler(404, page_not_found)
 
     return app
